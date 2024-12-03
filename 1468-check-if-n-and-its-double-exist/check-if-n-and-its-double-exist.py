ba52a9e53848e4f9dbac6a184cfd1a3b843c8386
Muhammad Ilyas <78874445+Muhammad-Ilyas-Ibrahim@users.noreply.github.com>
2024-12-04 00:14:21 +0500

    Time: 3 ms (42.42%) | Memory: 17.5 MB (12.13%) - LeetSync

--- 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.py
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.py
@@ -1,8 +1,10 @@
 class Solution:
     def checkIfExist(self, arr: List[int]) -> bool:
         visited = []
+
         for num in arr:
             if (num * 2 in visited) or (num % 2 == 0 and num // 2 in visited):
                 return True
             visited.append(num)
+
         return False
