36820d1e5b164c6b7d2c560f8c914cb1841113b3
Muhammad Ilyas <78874445+Muhammad-Ilyas-Ibrahim@users.noreply.github.com>
2024-12-28 00:12:00 +0500

    Time: 0 ms (100.00%) | Memory: 18.6 MB (6.20%) - LeetSync

--- 35-search-insert-position/search-insert-position.py
+++ 35-search-insert-position/search-insert-position.py
@@ -1,18 +1,9 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        dif = len(nums)
-        i = -1
 
         for index, n in enumerate(nums):
-            if target == n:
+            if n >= target:
                 return index
-            if (n - target) <= dif and (n-target) >= 1:
-                dif = n-target
-                i = index
-                print(f"n={n} | target={target} | i={i}")
         
-        if i >= 0:
-            return i
-        else:
-            return len(nums)
+        return len(nums)
 
